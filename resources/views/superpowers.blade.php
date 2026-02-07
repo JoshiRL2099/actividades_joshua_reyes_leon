e4e2ff6dee71894eb59f57319fea3f7186d0c397
JoshiRL <apato_2003@yahoo.com.mx>
2026-02-06 19:11:52 -0600

    initial Commmit

---
+++ resources/views/superpowers.blade.php
@@ -0,0 +1,65 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <body class="antialiased">
+            <table class="w-full border-collapse border border-gray-400">
+                <thead>
+                    <tr>
+                        <td>id</td>
+                        <td>Name</td>
+                        <td>Description</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Super Strength</td>
+                        <td>Ability to exert strength far beyond normal human limits.</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Enhanced Durability</td>
+                        <td>Resistance to physical damage and environmental hazards.</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Flight</td>
+                        <td>Ability to fly or levitate through the air.</td>
+                    </tr>
+                    <tr>    
+                        <td>4</td>
+                        <td>Telepathy</td>
+                        <td>Ability to read and communicate through thoughts.</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Telekinesis</td>
+                        <td>Ability to move objects with the mind.</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Super Speed</td>
+                        <td>Ability to move at speeds far beyond normal human capabilities.</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Shape Shifting</td>
+                        <td>Ability to change physical form and appearance.</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Regeneration</td>
+                        <td>Ability to heal rapidly from injuries.</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Energy Projection</td>
+                        <td>Ability to generate and manipulate energy for offensive or defensive purposes.</td>
+                    </tr>
+                    tr>
+                        <td>10</td>
+                        <td>Time Manipulation</td>
+                        <td>Ability to control or alter the flow of time.</td>
+                </tbody>
+            </table>
+            
+        </body>
