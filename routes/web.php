fd86fcaa7a848987abff0607023501acd573b4c4
JoshiRL <apato_2003@yahoo.com.mx>
2026-03-07 21:09:14 -0600

    actividad_7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Superhero;
+use App\Models\Universe;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,4 +18,92 @@ Route::get('/universes', function () {
 
 Route::get('/superpowers', function () {
     return view('superpowers');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Universe Routes
+|--------------------------------------------------------------------------
+*/
+
+// CREATE Universe
+Route::get('/universes/create', function () {
+    $universe = Universe::create([
+        'universe' => 'Marvel',
+        'company' => 'Marvel',
+        'age' => 'modern'
+    ]);
+
+    return response()->json([
+        'message' => 'Universe created successfully',
+        'universe' => $universe
+    ]);
+});
+
+// READ Universe with superheroes relation
+Route::get('/universes/read/{id}', function ($id) {
+    $universe = Universe::with('superheroes')->find($id);
+
+    return response()->json([
+        'universe' => $universe
+    ]);
+});
+
+// UPDATE Universe
+Route::get('/universes/update/{id}', function ($id) {
+    $universe = Universe::find($id);
+
+    $universe->update([
+        'age' => 'golden'
+    ]);
+
+    return response()->json([
+        'message' => 'Universe updated successfully',
+        'universe' => $universe
+    ]);
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Superhero Routes
+|--------------------------------------------------------------------------
+*/
+
+// CREATE Superhero
+Route::get('/superheroes/create', function () {
+    $superhero = Superhero::create([
+        'name' => 'Batman',
+        'real_name' => 'Bruce Wayne',
+        'gender' => 'male',
+        'universe_id' => 1
+    ]);
+
+    return response()->json([
+        'message' => 'Superhero created successfully',
+        'superhero' => $superhero
+    ]);
+});
+
+// READ Superhero with universe relation
+Route::get('/superheroes/read/{id}', function ($id) {
+    $superhero = Superhero::with('universe')->find($id);
+
+    return response()->json([
+        'superhero' => $superhero
+    ]);
+});
+
+// UPDATE Superhero
+Route::get('/superheroes/update/{id}', function ($id) {
+    $superhero = Superhero::find($id);
+
+    $superhero->update([
+        'gender' => 'female'
+    ]);
+
+    return response()->json([
+        'message' => 'Superhero updated successfully',
+        'superhero' => $superhero
+    ]);
 });
